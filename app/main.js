f4423b93d8e033439c8275907f69ca3233a98f85
Brian Hines <brian@projectweekend.net>
2014-12-10 05:40:45 -0600

    Re-organize main

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 var os = require( "os" );
+var async = require( "async" );
 var throng = require( "throng" );
 var connections = require( "./shared/connections" );
 var utils = require( "./shared/utils" );
@@ -12,25 +13,53 @@ var run = function () {
     var serialPort = connections.serialport();
     var broker = connections.jackrabbit();
 
+    var serialResponse = null;
+
+    var serialWrite = function ( data ) {
+        return function ( done ) {
+            serialResponse = null;
+            serialPort.write( data, done );
+        };
+    };
+
+    var serialDrain = function ( done ) {
+        serialPort.drain( done );
+    };
+
+    var getSerialResponse = function ( done ) {
+        var takingTooLong = false;
+        var start = new Date();
+        while ( !serialResponse && !takingTooLong ) {
+            var end = new Date();
+            takingTooLong = end - start > 500;
+        }
+
+        if ( serialResponse ) {
+            return done( null, serialResponse );
+        }
+        return done( new Error( "Serial response timeout" ) );
+    };
+
     var handleMessage = function ( message, ack ) {
-        serialPort.on( "data", function ( data ) {
-            ack( utils.parseSerialData( data ) );
-        } );
-        serialPort.write( message.serialMessage, function ( err, data ) {
+        async.series( [
+            serialWrite,
+            serialDrain,
+            getSerialResponse
+        ], function ( err, result ) {
             if ( err ) {
-                logger.log( "Error with 'serialPort.write': " + err );
-                process.exit();
+                logger.log( err );
+                ack();
+                process.exit( 1 );
             }
+            return ack( result );
         } );
     };
 
     var serve = function () {
-        logger.log( "Broker ready" );
         broker.handle( "sensor.get", handleMessage );
     };
 
     var create = function () {
-        logger.log( "Broker connected" );
         broker.create( "sensor.get", { prefetch: 5 }, serve );
     };
 
@@ -41,6 +70,9 @@ var run = function () {
     } );
 
     serialPort.on( "open", function () {
+        serialPort.on( "data", function ( data ) {
+            serialData = utils.parseSerialData( data );
+        } );
         logger.log( "Serial port open" );
         broker.once( "connected", create );
     } );
